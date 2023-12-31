bf94fc350c34cc9f7a76431a4837fc54da05be41
AyushR97j <ayushraj803110@gmail.com>
2023-12-31 22:08:18 +0530

    Added Video Container Section

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,23 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import VideoCard from './VideoCard';
 
 const VideoContainer = () => {
 
   const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
   const YOUTUBE_VIDEOS_API = process.env.REACT_APP_YOUTUBE_VIDEOS_API+GOOGLE_API_KEY
 
+  const [videos, setVideos] = useState([]);
+
   useEffect( () => {
       getVideos();
-  });
+  }, []);
 
   const getVideos = async () => {
       const data = await fetch(YOUTUBE_VIDEOS_API);
       const json = await data.json();
-      console.log(json);
+      setVideos(json.items);
   }
 
   return (
-    <div>
-      VideoContainer
+    <div className='flex flex-wrap'>
+    {videos.map( video => ( 
+    < VideoCard key={video.id} info={video}/> 
+    ))}
     </div>
   )
 }

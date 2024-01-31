764e85e7c6045032a47903ffcae72c565ed5e206
AyushR97j <ayushraj803110@gmail.com>
2024-02-01 00:22:37 +0530

    width fix in related videos section

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -7,13 +7,14 @@ const VideoCard = ({info}) => {
     //console.log("our url",url.get('v'));
     const isWatchPage = (url.get('v') != null);
     // console.log("info",info);
+    console.log("watch page",isWatchPage);
 
   return (
     <div className={`m-2 p-2 w-72 ${isWatchPage ? 'flex flex-row gap-3 p-2 w-10/12 h-32' : ''}`}>
       <img className='rounded-lg' alt="thumbnail" src={thumbnails.medium.url} />
       <div>
-        <div className={`font-bold py-0 line-clamp-2`}>{title}</div>
-        <div>{channelTitle}</div>
+        <div className={`font-bold py-0 line-clamp-2 pt-2 ${isWatchPage ? 'w-[80%]' : ''} `}>{title}</div>
+        <div className='line-clamp-1 w-64'>{channelTitle}</div>
         <div>{statistics?.viewCount} views</div>
       </div>
     </div>

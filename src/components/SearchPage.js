c32fdc8ffd07f2cd6ef418476fc1a67b9e99bd0a
AyushR97j <ayushraj803110@gmail.com>
2024-02-03 01:39:24 +0530

    Sidebar bug fix in WatchPage

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import SearchVideoCard from './SearchVideoCard';
+import {closeMenu} from "../utils/appSlice";
 
 const SearchPage = () => {
 
@@ -10,6 +11,8 @@ const SearchPage = () => {
     const searchQuery = searchParams.get('search_query');
     const [searchResults, setSearchResults] = useState(null);
     const theme = useSelector((store) => store.app.theme);
+    const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+    const dispatch = useDispatch();
 
     useEffect( () => {
         const searchQueryHandler = async() =>{
@@ -18,11 +21,13 @@ const SearchPage = () => {
             setSearchResults(json.items);
         };
 
+        dispatch(closeMenu())
+
         searchQueryHandler();
     }, [searchQuery]);
 
   return (
-    <div className={`w-full pl-6 pt-4 ${theme === 'dark' ? 'bg-black text-white' : ''}`}>
+    <div className={`w-full pl-6 pt-4 ${theme === 'dark' ? 'bg-black text-white' : ''} ${isMenuOpen ? 'ml-64' : ''}`}>
         {searchResults?.map( (searchResult, index) => (
             <Link className='flex-shrink' key={searchResult.id.videoId} to={"/watch?v="+searchResult.id.videoId} >
                 <SearchVideoCard key={index} info={searchResult} />
